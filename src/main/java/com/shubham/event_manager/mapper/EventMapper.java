65d6f3b29c2f69afa96bc815afe43351dd84863e
Shubhamkale1 <kaleshubham602@gmail.com>
2026-04-09 17:52:52 +0530

    new — MapStruct mapper interface

---
+++ src/main/java/com/shubham/event_manager/mapper/EventMapper.java
@@ -0,0 +1,17 @@
+package com.shubham.event_manager.mapper;
+
+
+import com.shubham.event_manager.dto.EventDTO;
+import com.shubham.event_manager.entity.Event;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface EventMapper {
+
+    EventDTO toDTO(Event event);
+
+    Event toEntity(EventDTO eventDTO);
+
+    void updateEventFromDTO(EventDTO eventDTO, @MappingTarget Event event);
+}
