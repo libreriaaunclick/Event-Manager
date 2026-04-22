32ba395b4f641b87c2df52ea557c5fb4aa917643
Shubhamkale1 <kaleshubham602@gmail.com>
2026-04-22 23:48:03 +0530

    feat: add searchEvents method to EventService interface

--- src/main/java/com/shubham/event_manager/service/EventService.java
+++ src/main/java/com/shubham/event_manager/service/EventService.java
@@ -1,16 +1,22 @@
 package com.shubham.event_manager.service;
 
+
+import com.shubham.event_manager.document.EventDocument;
 import com.shubham.event_manager.dto.EventDTO;
+
 import java.util.List;
 
 public interface EventService {
+
     List<EventDTO> getAllEvents();
 
     EventDTO getEventById(Long id);
 
-    EventDTO createEvent(EventDTO event);
+    EventDTO createEvent(EventDTO eventDTO, String userEmail);
 
-    EventDTO updateEvent(Long id, EventDTO event);
+    EventDTO updateEvent(Long id, EventDTO eventDTO);
 
     void deleteEvent(Long id);
+
+    List<EventDocument> searchEvents(String query);
 }
